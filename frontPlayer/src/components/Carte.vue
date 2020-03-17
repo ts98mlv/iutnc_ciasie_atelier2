76f3012ed2d029f2c41946e550f3cd436b291813
thebas57 <theohelf@gmail.com>
2020-03-17 22:55:33 +0100

    petite modif

--- frontPlayer/src/components/Carte.vue
+++ frontPlayer/src/components/Carte.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height: 500px; width: 100%">
-    <div style="height: 200px overflow: auto;">
+    <div style="height: 200px; overflow: auto;">
       <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
       <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p>
 
