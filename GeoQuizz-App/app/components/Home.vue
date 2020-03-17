b3787485ef8c0b81484a9e71cf6e328409dc7095
legrand82u <theo.legrand7@etu.univ-lorraine.fr>
2020-03-17 10:14:44 +0100

    Premier commit, initialisation app-mobile, prise de photo et sélection, upload vers imgBB, début codage de l'envoi vers l'API

---
+++ GeoQuizz-App/app/components/Home.vue
@@ -0,0 +1,144 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="GeoQuiZz"></Label>
+        </ActionBar>
+
+        <GridLayout columns="*,*">
+            <Button col="0" class="button" text="Prendre une photo" @tap="takePicture"/>
+            <Button col="1" class="button" text="Choisir une photo" @tap="selectPicture"/>
+            <WrapLayout class="test" col="1" row="2">
+                <Image class="test" v-for="(img,index) in images" :src="img.src" width="75" height="75"
+                       @tap="gererUpload(index)"/>
+            </WrapLayout>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    import * as camera from "nativescript-camera";
+    import * as imagepicker from "nativescript-imagepicker";
+    const bghttp = require("nativescript-background-http");
+    const session = bghttp.session("image-upload");
+    import {Image} from "tns-core-modules/ui/image";
+    import * as Geolocation from "nativescript-geolocation";
+
+    export default {
+        computed: {},
+        data() {
+            return {
+                images: [],
+                erreurLocation: false,
+                location: null,
+            }
+        },
+        methods: {
+            selectPicture() {
+
+                let context = imagepicker.create({
+                    mode: 'multiple'
+                });
+
+                context.authorize()
+                    .then(function () {
+                        return context.present();
+                    })
+                    .then(selection => {
+                        selection.forEach(selected => {
+
+                            console.log(JSON.stringify(selected));
+
+                            let img = new Image();
+                            img.src = selected;
+                            this.images.push(img);
+                        });
+                    }).catch(function (e) {
+                    console.log('error in selectPicture : ', e);
+                });
+
+            },
+            gererUpload(args) {
+                const key = "6bd91e436b3a802783b6bdea2bd530da";
+                let urlApi = "https://api.imgbb.com/1/upload?key=" + key;
+                let path = this.images[args].src._android;
+
+                const request = {
+                    url: urlApi,
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/octet-stream"
+                    },
+                    description: "Uploading"
+                };
+
+                const params = [
+                    {
+                        name: "image",
+                        filename: path,
+                        mimeType: "img/jpeg",
+                        localisation: "Nancy"
+                    }
+                ];
+
+                const task = session.multipartUpload(params, request);
+                task.on("complete",(res) => {
+                    alert(res.data);
+                    this.getLocation();
+                    alert(this.location);
+                });
+            },
+            takePicture() {
+                camera.requestPermissions()
+                    .then(() => {
+                        camera.takePicture({width: 300, height: 300, keepAspectRatio: true, saveToGallery: false})
+                            .then(imageAsset => {
+                                let img = new Image();
+                                img.src = imageAsset;
+                                this.images.push(img);
+                            })
+                            .catch(e => {
+                                console.log('error: ', e);
+                            });
+                    })
+                    .catch(e => {
+                        console.log('Error requesting permission');
+                    });
+            },
+            getLocation(){
+                Geolocation.getCurrentLocation({})
+                    .then((result) => {
+                        this.location = result;
+                    })
+                    .catch((error) => {
+                        alert("Erreur de récupération de la position, veuillez réessayer.");
+                    })
+            }
+        },
+        created() {
+            Geolocation.enableLocationRequest(true)
+                .then(() => {
+                    Geolocation.isEnabled().then( locActive => {
+                        if (!locActive){
+                            this.erreurLocationon = true;
+                            return;
+                        }
+                    })
+                });
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import '~@nativescript/theme/scss/variables/blue';
+
+    // Custom styles
+    .fas {
+        @include colorize($color: accent);
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
