e13f92b947717c9074493c6ad41dc2c1cf465ba4
thebas57 <theohelf@gmail.com>
2020-03-17 10:34:01 +0100

    Component Serie ok + ajout de vue-router

---
+++ frontPlayer/src/components/Play.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>COUCOU</h2>
+        <p>Salut ça va ?{{$route.params.id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
