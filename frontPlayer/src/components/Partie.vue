53259838d02b9ce94c0bac0fcfc2f7defce81522
thebas57 <theohelf@gmail.com>
2020-03-18 14:46:27 +0100

    json + map avancement

--- frontPlayer/src/components/Partie.vue
+++ frontPlayer/src/components/Partie.vue
@@ -2,6 +2,7 @@
   <div>
     <h3>Bonjour {{$route.params.pseudo}}</h3>
     <p>Vous avez choisis la série de {{listeSerie.ville}} avec une distance de {{listeSerie.distance}}</p>
+    <router-link v-bind:to="'/play/'+pseudo+ '/' + selected">Démarrer le jeu</router-link>
     <Carte></Carte>
   </div>
 </template>
@@ -14,9 +15,12 @@ export default {
   components: {
     Carte
   },
+  props: ["idPartie", "pseudo"],
   data() {
     return {
-      idPartie: this.$route.params.id
+      idPartie: this.$route.params.id,
+      selected: this.$route.params.id,
+      pseudo : this.$route.params.pseudo
     };
   },
   computed: {
@@ -25,6 +29,11 @@ export default {
         return element.id == this.idPartie;
       });
     }
+  },
+  created : function() {
+    this.idPartie = this.$route.params.id;
+    this.pseudo = this.$route.params.pseudo;
+
   }
 };
 </script>
