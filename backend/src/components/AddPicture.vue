926cdc7982a2121cbf6427d1712444e0f6308e49
allanizzi <allan_izzi@hotmail.com>
2020-03-18 15:47:10 +0100

    Debut affectation photos

--- backend/src/components/AddPicture.vue
+++ backend/src/components/AddPicture.vue
@@ -8,6 +8,7 @@
             <table class="tableauSeries">
                 <thead>
                     <tr>
+                        <th>Id</th>
                         <th>Ville</th>
                         <th>Map X</th>
                         <th>Map Y</th>
@@ -16,6 +17,7 @@
                     </tr>
                 </thead>
                     <tr v-for="serie in listOfDatasSeries">
+                        <td>{{serie.id}}</td>
                         <td>{{serie.ville}}</td>
                         <td>{{serie.map_x}}</td>
                         <td>{{serie.map_y}}</td>
@@ -37,13 +39,15 @@
                         <th>positionX</th>
                         <th>positionY</th>
                         <th>Photo</th>
+                        <th>Serie</th>
                     </tr>
                 </thead>
                     <tr v-for="photo in listOfDatasPhotos">
                         <td>{{photo.description}}</td>
-                        <td>{{photo.positionX}}</td>
-                        <td>{{photo.positionY}}</td>
+                        <td>{{photo.position_x}}</td>
+                        <td>{{photo.position_y}}</td>
                         <td><img :src='photo.url'></td>
+                        <td>{{photo.serie_id}}</td>
                     </tr>
 
                 <tbody>
@@ -51,40 +55,73 @@
                 </tbody>
             </table>
         </div>
+
+        <form id="affectPhoto" @submit="checkForm" action="/something" method="post">
+  
+            <p v-if="errors.length">
+                <b>Veuillez remplir ces champs:</b>
+                <ul>
+                <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+            
+            <p>
+                <label for="ville">Ville<label>
+                <input type="text" name="ville" id="ville" v-model="ville">
+            </p>
+
+            <p>
+                <label for="mapX">Map_x<label>
+                <input type="number" name="age" id="age" v-model="age" min="0">
+            </p>
+
+            <p>
+                <label for="movie">Favorite Movie<label>
+                <select name="movie" id="movie" v-model="movie">
+                <option>Star Wars</option>
+                <option>Vanilla Sky</option>
+                <option>Atomic Blonde</option>
+                </select>
+            </p>
+
+            <p>
+                <input type="submit" value="Submit">  
+            </p>
+
+        </form>
     </div>
-    
 </template>
 
-
 <script>
-import datas from '../../local-data.json';
+import datasSeries from '../assets/serie.json'
+import datasPhotos from '../assets/photo.json'
 
 export default {
-    name: 'addpho',
-    data() {
-        return {
-            series: []
-        }
-    },
-
-    methods: {
-
-    },
-
-    computed: {
-
-        listOfDatasSeries() {
-            return datas.series.map(item => {
-                return item;
-            });
-        },
-
-        listOfDatasPhotos() {
-            return datas.photos.map(item => {
-                return item;
-            });
-        }
+  name: 'addpho',
+  data () {
+    return {
+      series: []
     }
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+
+    listOfDatasSeries () {
+      return datasSeries.series.map(item => {
+        return item
+      })
+    },
+
+    listOfDatasPhotos () {
+      return datasPhotos.photos.map(item => {
+        return item
+      })
+    }
+  }
 }
 
 </script>
