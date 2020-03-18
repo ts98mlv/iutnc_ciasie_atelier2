2816dd5ff0ec0b94ca48f2bddc1b9d993de51ea4
thebas57 <theohelf@gmail.com>
2020-03-19 00:00:53 +0100

    avancement option carte.........

---
+++ frontPlayer/src/components/Cartecrassier.vue
@@ -0,0 +1,102 @@
+<template >
+  <div style="height: 500px; width: 100%">
+    <div style="height: 200px; overflow: auto;">
+      <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
+      <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p>
+      <p v-if="this.partieStatus==1">Nb photo :{{findPartie.nb_photos}}</p>
+      <button @click="showMap = !showMap">Toggle map</button>
+    </div>
+    <l-map
+      v-if="showMap"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      style="height: 80%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer :url="url" :attribution="attribution" />
+      <l-marker :lat-lng="withPopup">
+        <l-popup>
+          <div @click="innerClick">Cinéma</div>
+        </l-popup>
+      </l-marker>
+      <l-marker :lat-lng="withTooltip">
+        <l-tooltip :options="{ permanent: true, interactive: true }">
+          <div @click="innerClick">I am a tooltip</div>
+        </l-tooltip>
+      </l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import serie from "../assets/serie.json";
+import partie from "../assets/partie.json";
+
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
+
+export default {
+  name: "Example",
+
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 13,
+      partieStatus: 0,
+      center: latLng(0, 0),
+      idPartie: this.$route.params.id,
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(0, 0),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 11.5,
+      currentCenter: latLng(47.41322, -1.219482),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    innerClick() {
+      alert("Click!");
+    }
+  },
+  created: function() {
+    this.center = latLng(this.listeSerie.map_x, this.listeSerie.map_y);
+    this.withPopup = latLng(49.132728, 6.198789);
+    this.partieStatus = 1;
+  },
+  computed: {
+    listeSerie() {
+      return serie.series.find(element => {
+        return element.id == this.idPartie;
+      });
+    },
+    findPartie() {
+      return partie.parties.find(element => {
+          return element.serie_id == this.idPartie;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
