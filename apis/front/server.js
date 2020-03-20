9352708eb1eecb5b9f8b0405a5a952a5c4a9ae22
thomas SERRES <t.srs@free.fr>
2020-03-20 10:15:10 +0100

    FrontOffice API : Création de partie

--- apis/front/server.js
+++ apis/front/server.js
@@ -41,16 +41,44 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors(corsOptions));
 
 /**
- * route d'accueil de l'api commande
- * affiche un message de bienvenue
- * @root /
- * @method get
+ * @api  {get} / route d'accueil de l'api commande
+ * @apiDescription affiche un message de bienvenue
  */
 app.get("/", (req, res) => {
     res.status(200).end("Bienvenue sur GeoQuizz API du front office ");
 });
 
+/**
+ * @api {post} /parties crée une nouvelle partie en fonction d'un json fourni
+ * @apiDescription crée une nouvelle partie en fonction d'un json fourni
+ * @apiParam {json} body {
+ *     "nb_photos" : 1,
+ *     "joueur_id" : 1
+ * }
+ * @apiSuccess {json} result fichier json avec les données fournies en entrée (jsonFourni) et le token de la partie (token)
+ */
+app.post("/parties", (req, res) => {
+   //vérification des données envoyées
+   let jsonPartie = req.body;
+   let nb_photos = jsonPartie.nb_photos;
+   let joueur_id = jsonPartie.joueur_id;
 
+   if(isUndefined(nb_photos) || isUndefined(joueur_id) || !isStrictlyPositive(nb_photos) || !isPositive(joueur_id)){
+       res.status(500).end(getMessageFromHTTPCode(666));
+   }
+
+   //génération du token uuid
+    let token = uuid();
+
+   //enregistrement en bdd
+    db.query("insert into partie (`token`, `nb_photos`, `joueur`) values (?, ?, ?)", [token, nb_photos, joueur_id], (err, result) => {
+        if(err){
+            res.status(500).end(getMessageFromHTTPCode(500));
+        }
+        res.status(200).end(JSON.stringify({jsonFourni : jsonPartie, token: token}));
+    })
+
+});
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                  Fin des routes                                                                    //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -90,3 +118,61 @@ db.connect(err => {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                          Fonctions                                                                 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function isUndefined(element) {
+    return (typeof element === "undefined");
+}
+
+function isEmptyString(element){
+    return (element === "");
+}
+
+function isNumber(element){
+    return (typeof element === "number");
+}
+
+function isPositive(element) {
+    return (isNumber(element) && element >= 0);
+}
+
+function isStrictlyPositive(element) {
+    return (isNumber(element) && element > 0);
+}
+
+/**
+ * fonction qui retourne un json avec le code d'erreur et le message
+ * @param code
+ * @returns json
+ */
+function getMessageFromHTTPCode(code) {
+    code = parseInt(code);
+    let message = "";
+
+    switch (code) {
+        case 400:
+            message = "Requête invalide : mauvaise requête ou paramètre manquant";
+            break;
+        case 401:
+            message = "Accès non autorisé / Utilisateur non connecté";
+            break;
+        case 403:
+            message = "Accès interdit, vous n'avez pas les droits suffisants";
+            break;
+        case 404:
+            message = "Ressource non trouvée";
+            break;
+        case 500:
+            message = "Erreur du serveur";
+            break;
+        case 200:
+            message = "Tout s'est bien passé :)";
+            break;
+        case 666:
+            message = "JSON non conforme !!";
+            break;
+    }
+
+    message = JSON.stringify({code: code, message: message});
+
+    return message;
+}
+
