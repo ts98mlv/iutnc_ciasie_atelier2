e40d74bc1162cb448cfa69932843ae35d577cbef
thebas57 <theohelf@gmail.com>
2020-03-26 22:35:29 +0100

    On lie l'aapi comme on peut

--- frontPlayer/src/components/Finish.vue
+++ frontPlayer/src/components/Finish.vue
@@ -5,14 +5,14 @@
 </template>
 
 <script>
-
+const axios = require("axios");
 
 export default {
 
   data() {
     return {
       idPartie: this.$route.params.id,
-      selected: this.$route.params.id,
+      idSerie: this.$route.params.serid,
       pseudo: this.$route.params.pseudo,
       score : this.$route.params.score
     };
@@ -21,7 +21,21 @@ export default {
 
   },
   created: function() {
-
+     let jsonEnvoi = {
+        score: this.score,
+        nb_photos : 5
+      };
+      axios({
+        method: "PUT",
+        url: "http://docketu.iutnc.univ-lorraine.fr:17180/parties/" + this.idPartie,
+        data: jsonEnvoi
+      })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
   }
 };
 </script>
