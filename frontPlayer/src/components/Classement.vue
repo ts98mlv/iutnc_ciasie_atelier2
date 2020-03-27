2ef1c7635781d32ad2f0bdb1cf132c117f0d5f29
thebas57 <theohelf@gmail.com>
2020-03-27 12:09:58 +0100

    fin partie bledard mais ok

---
+++ frontPlayer/src/components/Classement.vue
@@ -0,0 +1,41 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+const axios = require("axios");
+
+export default {
+  data() {
+    return {
+      idSerie: this.$route.params.serid
+    };
+  },
+  created: function() {
+    // Recup les parties avec le bon id serie qui correspond
+    axios
+      .get(
+        "http://docketu.iutnc.univ-lorraine.fr:17180/parties/" + this.idPartie
+      )
+
+      //.get("https://58de787a.ngrok.io/parties/" + this.idPartie)
+
+      .then(response => {
+        // handle success
+        console.log(response);
+        this.tokenSerie = response.data[0].token;
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function() {
+        // always executed
+      });
+  }
+};
+</script>
+
+<style>
+@import "../style/css/End.css";
+</style>
--- frontPlayer/src/main.js
+++ frontPlayer/src/main.js
@@ -6,6 +6,7 @@ import Partie from './components/Partie.vue';
 import Serie from './components/Serie.vue';
 import Play from './components/Play.vue';
 import Finish from './components/Finish.vue';
+import Classement from './components/Classement.vue';
 import Carte from './components/Carte.vue';
 
 Vue.use(VueRouteur)
@@ -17,7 +18,8 @@ const router = new VueRouteur ({
     {path: '/partie/:serid/:id', component:Partie},
     {path: '*',redirect:'/'},
     {path: '/play/:serid/:id', component:Play},
-    {path: '/end/:serid/:id/:score', component:Finish},
+    {path: '/end/:serid/:id/:score/:token/:nbphoto', component:Finish},
+    {path: '/score/:serid', component:Classement},
   ]
 })
 
